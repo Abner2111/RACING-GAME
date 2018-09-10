1f1d412fa2d7e8ee649e4a5d6325fe7002c88bad
[Abner] <[abnerAQ73@gmail.com]>
2018-09-09 22:12:55 -0600

    Finished about page.(have to fux image bug)

--- Interface.py
+++ Interface.py
@@ -9,10 +9,16 @@ import os
 import winsound
 from tkinter import ttk
 
+#funcion para cargar imágenes
 def cargarImg(nombre):
-    ruta=os.path.join('img',nombre)
+    ruta=os.path.join('img', nombre)
     imagen=PhotoImage(file=ruta)
     return imagen
+
+#función para volver a la pantalla inicial
+def Main(Window):
+    Window.withdraw()
+    root.deiconify()
 
 #Ventana principal
 root = Tk()
@@ -40,19 +46,37 @@ def About():
     C_about = Label(about, bg="white", width=1280, height=720)
     C_about.place(x=0,y=0)
 
-    tec = Label(C_about, text="INSTITUTO TECNOLÓGICO DE COSTA RICA", bg="white", font = ("times new roman", 28)).place(y=0,x=50)
-    Salir = ttk.Button(C_about, text="SALIR", command = lambda :Main(about))
-    Salir.place(x=0,y=690)
+    #Labels con informacion de About
+    tec = Label(C_about, text="INSTITUTO TECNOLÓGICO DE COSTA RICA", bg="white", font = ("times new roman", 28))
+    tec.place(y=0, x=25)
+    carrera = Label(C_about, text="Carrera: Ingeniería en computadores", bg="white", font=("times new roman", 28))
+    carrera.place(y=50, x=25)
+    curso = Label(C_about, text="Curso: Taller de programación", bg="white", font=("times new roman", 28))
+    curso.place(y=100, x=25)
+    profesor = Label(C_about, text="Profesor: Milton Villegas Lemus", bg="white", font=("times new roman", 28))
+    profesor.place(y=150, x=25)
+    Dios = Label(C_about, text="Estudiante: Abner Arroyo Quesada", bg="white", font=("times new roman", 28))
+    Dios.place(y=200, x=25)
+    carnet = Label(C_about, text="Carnet: 2018103035", bg="white", font=("times new roman", 28))
+    version = Label(C_about, text="Version: 1.0", bg="white", font=("times new roman", 28))
+    ult_modc = Label(C_about, text="Última modificación: ND", bg="white", font=("times new roman", 28))
+    ult_modc.place(y=250, x=25)
+    ruta = os.path.join('img',"Foto.gif")
+    img_foto = imagen=PhotoImage(file=ruta)
+    foto = ttk.Label(C_about, image=img_foto)
+    foto.place(y=50, x=700)
+    #Botón de salir
+    Salir = ttk.Button(C_about, text="SALIR", command=lambda: Main(about))
+    Salir.place(x=1198, y=690)
+
+def Juego():
 
 
-def Main(Window):
-    Window.withdraw()
-    root.deiconify()
 
 #BOTONES
 img_play = cargarImg("play.gif")
 img_about = cargarImg("info.gif")
-play = Button(C_main, image = img_play)
+play = Button(C_main, image=img_play)
 play.place(x=465, y=310)
 
 about = Button(C_main, image = img_about, command = lambda : About())
