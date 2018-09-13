b79f7f16c8008ddda3d604a9145311a60d0e02b6
[Abner] <[abnerAQ73@gmail.com]>
2018-09-12 23:19:18 -0600

    Inicio de interfaz de Juego y addition de elementos en interfaz de pantalla de inicio

--- Interface.py
+++ Interface.py
@@ -6,7 +6,7 @@ from threading import Thread
 import threading
 import time
 import os
-#import winsound
+import winsound
 from tkinter import ttk
 
 #funcion para cargar imágenes
@@ -19,6 +19,10 @@ def cargarImg(nombre):
 def Main(Window):
     Window.withdraw()
     root.deiconify()
+def Game():
+    nombre = str(E_nombre.get())
+    Juego(nombre)
+
 
 #Ventana principal
 root = Tk()
@@ -34,6 +38,11 @@ img_titulo = cargarImg("titulo.gif")
 Titulo = Label(C_main,image=img_titulo, bg="white"\
                , borderwidth=0, highlightthickness=0)
 
+#Entrada de texto para nombre del jugador
+L_nombre = Label(C_main, text="Ingrese su nombre", font=("bauhaus 93", 18), width=27, bg="#DED7DE")
+L_nombre.place(x=465, y=210)
+E_nombre = Entry(C_main, text="Ingrese su nombre", width=59, bg="#DED7DE", fg="black")
+E_nombre.place(x=465, y=260)
 
 Titulo.place(y=0,x=0)
 
@@ -62,24 +71,36 @@ def About():
     version = Label(C_about, text="Version: 1.0", bg="white", font=("times new roman", 28))
     ult_modc = Label(C_about, text="Última modificación: ND", bg="white", font=("times new roman", 28))
     ult_modc.place(y=250, x=25)
-    img_foto = cargarImg("Foto.gif")
-    foto = Label(C_about, image=img_foto, bg="white")
-    foto.photo=img_foto
+    ruta = os.path.join('img',"Foto.gif")
+    img_foto = imagen=PhotoImage(file=ruta)
+    foto = ttk.Label(C_about, image=img_foto)
+    foto.photo = img_foto
     foto.place(y=50, x=700)
     #Botón de salir
     Salir = ttk.Button(C_about, text="SALIR", command=lambda: Main(about))
     Salir.place(x=1198, y=690)
 
-def Juego():
-    return None
+def Juego(nombre):
+    root.withdraw()
+
+    juego = Toplevel()
+    juego.minsize(1280, 720)
+    juego.resizable(width=NO, height=NO)
+
+    C_game = Canvas(juego, bg="#DED7DE", width=1280, height=720)
+    C_game.place(y=0, x=0)
+    test = Label(C_game, text=nombre)
+    test.pack()
+
 
 
 
 #BOTONES
 img_play = cargarImg("play.gif")
 img_about = cargarImg("info.gif")
-play = Button(C_main, image=img_play)
+play = Button(C_main, image=img_play, command = lambda : Game())
 play.place(x=465, y=310)
+
 
 about = Button(C_main, image = img_about, command = lambda : About())
 about.place(y = 630, x = 1190)
